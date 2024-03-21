1909533db9ed88c1c4d5878a2d20b8fbc0951ba7
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2024-03-21 23:44:10 +0200

    fix stop app bug

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,24 +1,92 @@
 package com.example.calculator;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView resulttext;
+    EditText firstnum, secondnum;
+    Button addbutton, subbutton, mulbutton, divbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        firstnum = findViewById(R.id.num1);
+        secondnum = findViewById(R.id.num2);
+        resulttext = findViewById(R.id.result);
+        addbutton = findViewById(R.id.add);
+        subbutton = findViewById(R.id.Subtract);
+        mulbutton = findViewById(R.id.multiply);
+        divbutton = findViewById(R.id.Divide);
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addtwonumber();
+            }
         });
+
+        subbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subtwonumber();
+            }
+        });
+
+        mulbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                multwonumber();
+            }
+        });
+
+        divbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                divtwonumber();
+            }
+        });
+    }
+
+    private void addtwonumber() {
+        int num1, num2, sum;
+        num1 = Integer.parseInt(firstnum.getText().toString());
+        num2 = Integer.parseInt(secondnum.getText().toString());
+        sum = num1 + num2;
+        resulttext.setText("Result: " + sum);
+    }
+
+    private void subtwonumber() {
+        int num1, num2, sub;
+        num1 = Integer.parseInt(firstnum.getText().toString());
+        num2 = Integer.parseInt(secondnum.getText().toString());
+        sub = num1 - num2;
+        resulttext.setText("Result: " + sub);
+    }
+
+    private void multwonumber() {
+        int num1, num2, mul;
+        num1 = Integer.parseInt(firstnum.getText().toString());
+        num2 = Integer.parseInt(secondnum.getText().toString());
+        mul = num1 * num2;
+        resulttext.setText("Result: " + mul);
+    }
+
+    private void divtwonumber() {
+        int num1, num2, div;
+        num1 = Integer.parseInt(firstnum.getText().toString());
+        num2 = Integer.parseInt(secondnum.getText().toString());
+        if(num2 != 0) {
+            div = num1 / num2;
+            resulttext.setText("Result: " + div);
+        } else {
+            resulttext.setText("Cannot divide by zero");
+        }
     }
 }
