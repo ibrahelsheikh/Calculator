f8247fb43a5bf11ba320a40b2f5c9b8d790bc3be
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2024-03-22 21:17:51 +0200

    history button leads to SecondActivity

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView resulttext;
     EditText firstnum, secondnum;
-    Button addbutton, subbutton, mulbutton, divbutton;
+    Button addbutton, subbutton, mulbutton, divbutton, historybutton;
+    File calculationFile;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         subbutton = findViewById(R.id.Subtract);
         mulbutton = findViewById(R.id.multiply);
         divbutton = findViewById(R.id.Divide);
+        historybutton = findViewById(R.id.history);
 
         addbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +68,13 @@ public class MainActivity extends AppCompatActivity {
                 divtwonumber();
             }
         });
+
+        historybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
     }
 
     private void addtwonumber() {
@@ -60,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         num2 = Integer.parseInt(secondnum.getText().toString());
         sum = num1 + num2;
         resulttext.setText("Result: " + sum);
+        saveCalculationToFile(num1 + " + " + num2 + " = " + sum);
     }
 
     private void subtwonumber() {
@@ -68,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         num2 = Integer.parseInt(secondnum.getText().toString());
         sub = num1 - num2;
         resulttext.setText("Result: " + sub);
+        saveCalculationToFile(num1 + " - " + num2 + " = " + sub);
     }
 
     private void multwonumber() {
@@ -76,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         num2 = Integer.parseInt(secondnum.getText().toString());
         mul = num1 * num2;
         resulttext.setText("Result: " + mul);
+        saveCalculationToFile(num1 + " * " + num2 + " = " + mul);
     }
 
     private void divtwonumber() {
@@ -85,8 +111,55 @@ public class MainActivity extends AppCompatActivity {
         if(num2 != 0) {
             div = num1 / num2;
             resulttext.setText("Result: " + div);
+            saveCalculationToFile(num1 + " / " + num2 + " = " + div);
         } else {
             resulttext.setText("Cannot divide by zero");
         }
+    }
+
+    private void saveCalculationToFile(String calculation) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE
+            );
+        } else {
+            try {
+                String fileName = "calculations.txt";
+                calculationFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+                FileOutputStream fileOutputStream = new FileOutputStream(calculationFile, true);
+                fileOutputStream.write((calculation + "\n").getBytes());
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Unable to save calculation", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Need permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/calculator/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.textView2);
+        String calculations = getIntent().getStringExtra("calculations");
+        textView.setText(calculations);
+    }
+}
